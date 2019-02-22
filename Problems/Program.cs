3866bcefcfed20c1691a66ced2220b2ea1f20aee
Minali <minali.chouhan@gmail.com>
2019-02-22 19:00:54 +0200

    Added more files

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,39 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
+
     {
+
+        public string Prefix(string input)
+        {
+           
+            int len=input.Length;
+            
+            string temp=input;
+            string [] totalwords=temp.Split(" ");
+            int counter=totalwords.Length;
+                if(input=="")
+            {
+             int total=0;
+                return ($"{len},{total}:{input}");
+            }
+
+            foreach(string s in totalwords)
+            {
+                if(s=="")
+                {
+                    counter--;
+                }
+            }
+                return ($"{len},{counter}:{input}");
+
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+           
         }
     }
 }
